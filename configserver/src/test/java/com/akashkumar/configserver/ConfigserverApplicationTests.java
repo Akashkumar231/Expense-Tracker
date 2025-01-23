a1a00ddc46985cdccc07a2ffb389f635781c0080
akashkumaryadav <ay355231@gmail.com>
2025-01-23 17:04:38 +0530

    add configserver microservice

---
+++ configserver/src/test/java/com/akashkumar/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akashkumar.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
